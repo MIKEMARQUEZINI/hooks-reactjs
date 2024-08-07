67f90de3997cee9d9f2381a3bc3e3eec9709af14
Mike Marquezini <mike.santosmarquezini@ab-inbev.com>
2024-08-06 21:19:55 -0300

    feat: object to simulate the api response

---
+++ src/components/utils/data.ts
@@ -0,0 +1,69 @@
+export interface People {
+  name: string;
+  age: number;
+  profession: string;
+  isAdult: boolean;
+}
+
+export const person: People[] = [
+  {
+    name: "João",
+    age: 25,
+    profession: "Engenheiro",
+    isAdult: true,
+  },
+  {
+    name: "Maria",
+    age: 17,
+    profession: "Estudante",
+    isAdult: false,
+  },
+  {
+    name: "Carlos",
+    age: 30,
+    profession: "Médico",
+    isAdult: true,
+  },
+  {
+    name: "Ana",
+    age: 22,
+    profession: "Designer",
+    isAdult: true,
+  },
+  {
+    name: "Luiza",
+    age: 16,
+    profession: "Estudante",
+    isAdult: false,
+  },
+  {
+    name: "Pedro",
+    age: 28,
+    profession: "Advogado",
+    isAdult: true,
+  },
+  {
+    name: "Fernanda",
+    age: 19,
+    profession: "Programadora",
+    isAdult: true,
+  },
+  {
+    name: "Bruno",
+    age: 35,
+    profession: "Professor",
+    isAdult: true,
+  },
+  {
+    name: "Aline",
+    age: 21,
+    profession: "Arquiteta",
+    isAdult: true,
+  },
+  {
+    name: "Gustavo",
+    age: 15,
+    profession: "Estudante",
+    isAdult: false,
+  },
+];
